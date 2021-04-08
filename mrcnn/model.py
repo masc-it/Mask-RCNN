2ce7296e88bf95eb839a256a4488d19f71c2dae4
onipot <m.sciancalepore20@studenti.uniba.it>
2021-04-08 10:46:05 +0200

    save only best epochs (min val_loss)

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2330,9 +2330,9 @@ class MaskRCNN(object):
 
         # Callbacks
         callbacks = [
-            keras.callbacks.TensorBoard(log_dir=self.log_dir,
-                                        histogram_freq=0, write_graph=True, write_images=False),
-            keras.callbacks.ModelCheckpoint(self.checkpoint_path,
+            #keras.callbacks.TensorBoard(log_dir=self.log_dir,
+            #                            histogram_freq=0, write_graph=True, write_images=False),
+            keras.callbacks.ModelCheckpoint(self.checkpoint_path, monitor="val_loss", mode="min",
                                             verbose=0, save_weights_only=True),
         ]
 
